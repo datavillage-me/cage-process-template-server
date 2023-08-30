03782ffd1fd620eb0c7c368bd09bdcbca37c8bdb
Loic Quertenmont <loic.quertenmont@gmail.com>
2023-08-30 14:37:49 +0200

    feat: first version

--- README.md
+++ README.md
@@ -1,41 +1,24 @@
 # Template for a process to be deployed in the data cage
 
-This is a sample project with a representative set of resources to deploy a data processing machine learning algorithm in a Datavillage cage.
-The example is using XGBoost library and its classification example: https://xgboost.readthedocs.io/en/stable/get_started.html
+This is a sample project with a representative set of resources to deploy a simple web server in a Datavillage cage.
+The example is using FAST API library for the web server, but any other tool such as Flask, Django, etc. would behave similarly
 
-__TL;DR__ : clone this repo and edit the `process.py` file to adapt it for your own use casse
+__TL;DR__ : clone this repo and edit the `index.py` file to adapt it for your own use casse
 
 
 ## Template Use case
 
 
 To make this template closer to a real use case,
-it implements 3 event processing logic
- 1. TRAIN event to train a classification model on a data batch
- 2. PREDICT event to predict classification classes on a data batch using a previously trained event
- 3. INFER event to infer (predict) a classification class on one single case at a time
+it implements 3 types of request handling examples
+ 1. GET "/" route returns a hello World message
+ 2. GET "/users/" route returns the list of users connected to the collaboration space
+ 3. POST "/log/" route expects to receive a log message that is pushed on the audit log stack
 
 All these steps are defined in distinct functions of the process.py file.
 Simply adapt this function when addressing your own use case
-
 
 ## Content of this repo
 
 Have a look at the main template repository to get familiar with the template structure
 https://github.com/datavillage-me/cage-process-template
-
-## Infering event
-
-For testing the inference, you can send an event with that content:
-```
-{
-   "type": "INFER",
-   "data": {
-      "sepal length (cm)": 5.9,
-      "sepal width (cm)": 3.0,
-      "petal length (cm)": 5.1,
-      "petal width (cm)": 1.8
-   }
-}
-```
-You should see in the log of the algorithm that it is categorize as a "virginica" Iris species
--- index.py
+++ index.py
@@ -1,12 +1,90 @@
 """
-Exemple file for processing dv events in a datacage and pushing the output to the user pod
+Exemple file for starting a web server in a datacage and answering to requests
 """
-from dv_utils import DefaultListener
+import asyncio
+import os
+import pathlib
+import signal
+import sys
+import typing
+from typing import Any, TypedDict
 
-from process import event_processor, default_settings
+import uvicorn
+from fastapi import (
+    APIRouter,
+    BackgroundTasks,
+    FastAPI,
+    HTTPException,
+    Request,
+    UploadFile,
+    status,
+)
+from dv_utils import default_settings, Client, audit_log
 
-default_settings.daemon = True
-print("DEFAULT SETTINGS", default_settings)
+##################
 
-DefaultListener(event_processor, daemon=default_settings.daemon)
+APP_ID = os.environ.get("DV_CAGE_ID",None)
+ROOT_PATH = ""
+if "DV_CAGE_ID" in os.environ:
+    ROOT_PATH = "/" + os.environ["DV_CAGE_ID"].split("-")[-1]
+else:
+    #    ROOT_PATH = "/CAGE_ID"
+    ROOT_PATH = ""
+print("ROOT_PATH=", ROOT_PATH)
 
+app = FastAPI(
+    title="webserver-in-cage",
+    description="""
+        datavillage template of a web server running in a datacage
+    """,
+    root_path=ROOT_PATH,
+)
+app_router = APIRouter(prefix=ROOT_PATH)
+
+
+@app.get("/")
+async def root():
+    """
+    Route endpoint return an "hello world" response
+    """
+    return {"message": f"Hello world from a data cage"}
+
+
+@app.post("/log")
+async def push_log(log: str = "empty log"):
+    """
+    Example of a route receiving a log string and pushing it to the log queue
+    """
+    audit_log(APP_ID, f"upload_data {in_file.filename}")
+    return {"message": f"The log '{log}' was properly pushed to the log stack"}
+
+@app.get("/users")
+async def list_users():
+    """
+    Route endpoint that return the list of users connected to the collaboration space
+    """
+    client = Client()
+    user_ids = client.get_users()
+    
+    return {"user_ids": user_ids}
+
+
+async def web_server():
+    """
+    Start the web server
+    """
+    audit_log(APP_ID, "web server starting")
+
+    config = uvicorn.Config(app, port=80, host="0.0.0.0", reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+    audit_log(APP_ID, "web server closing")
+
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+    except Exception:  # pylint: disable=broad-except
+        loop = asyncio.new_event_loop()
+    loop.run_until_complete(web_server())
